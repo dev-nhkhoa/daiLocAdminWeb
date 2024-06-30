f24d79244f0072fd66312453a8158976016085f4
dev-nhkhoa <work.nhkhoa@gmail.com>
2024-06-30 21:48:00 +0700

    fix bug

--- src/lib/date.js
+++ src/lib/date.js
@@ -4,7 +4,7 @@ function formattedDate() {
   const minute = date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes()
 
   const day = date.getDate()
-  const month = date.getMonth() + 1
+  const month = date.getMonth() < 10 ? `0${date.getMonth() + 1}` : date.getMonth() + 1
   const year = date.getFullYear()
 
   return `${hour}:${minute} ${day}/${month}/${year}`
